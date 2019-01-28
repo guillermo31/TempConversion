d98b571ea03aa3cd319e11fa9401f1866628f288
Guillermo Ramos <memo33101@gmail.com>
2019-01-28 10:31:52 -0700

    Initial Commit

---
+++ TempConversion/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TempConversion
+//
+//  Created by Guillermo Ramos  on 1/28/19.
+//  Copyright © 2019 Guillermo Ramos . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
